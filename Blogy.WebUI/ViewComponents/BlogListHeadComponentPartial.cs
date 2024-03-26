bc128b6f84dacf848089540a0f18ceb96a7c42ae
Faruk Çelik <ahmet48.faruk@gmail.com>
2024-03-26 15:36:36 +0300

    #

---
+++ Blogy.WebUI/ViewComponents/BlogListHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.viewComponents
+{
+	public class BlogListHeadComponentPartial: ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ Blogy.WebUI/ViewComponents/BlogListNavbarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.ViewComponents
+{
+	public class BlogListNavbarComponentPartial: ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+		
+	}
+}
